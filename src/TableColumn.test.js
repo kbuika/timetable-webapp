5234a1d6922f238b46879d42a91b86a90517d673
kibuikaCodes <kibuikasteve@gmail.com>
2020-09-20 17:24:58 +0300

    :sparkles: feature: add static data and load it to the table

--- src/TableColumn.test.js
+++ src/TableColumn.test.js
@@ -4,6 +4,8 @@ import TableColumn from "./components/TableColumn";
 
 test("renders correctly the table columns", () => {
   const title = "Title";
-  const tree = renderer.create(<TableColumn title={title} />).toJSON();
+  const tree = renderer
+    .create(<TableColumn title={title} lesson={"lesson5"} />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
